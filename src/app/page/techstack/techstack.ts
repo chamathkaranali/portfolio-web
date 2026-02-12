9c9284785576ff5763a251d51671e42bf2a47d93
Chamathka Ranali <ranalichamathka@gmail.com>
2026-02-12 11:51:52 +0530

    Add About, Contact, Home, Project, and Techstack components with associated styles and tests

---
+++ src/app/page/techstack/techstack.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-techstack',
+  imports: [],
+  templateUrl: './techstack.html',
+  styleUrl: './techstack.css',
+})
+export class Techstack {
+
+}
